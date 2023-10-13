55eb59598845998511d393dd0598d58115fc269e
Brian Klein <bklein1981@gmail.com>
2023-10-12 21:34:53 -0500

    renamed index.js to server.js, fixed package.json

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectionString =
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB');
-
-connect(connectionString);
 
 module.exports = connection;
